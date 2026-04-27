6381c56f9ae06709cc55a9590c7a5b7d907070f1
Julian Pollak <j.pollak@fruitcake.ch>
2026-04-27 23:53:23 +0200

    migrate project to ES modules

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-const { app, BrowserWindow, Tray, Menu, session } = require('electron');
-const path = require('path');
+import { app, BrowserWindow, Tray, Menu, session } from 'electron';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.commandLine.appendSwitch('use-gl', 'angle');
 app.commandLine.appendSwitch('use-angle', 'vulkan');
